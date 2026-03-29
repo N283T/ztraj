be777e389206a3944d8d8ed7c1e05e53f93972d9
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-29 21:53:09 +0900

    release: v0.6.0

--- src/c_api.zig
+++ src/c_api.zig
@@ -55,7 +55,7 @@ pub const ZTRAJ_ERROR_EOF: c_int = -5;
 // Version
 // =============================================================================
 
-const VERSION: [*:0]const u8 = "0.5.1";
+const VERSION: [*:0]const u8 = "0.6.0";
 
 /// Get the library version string.
 export fn ztraj_version() callconv(.c) [*:0]const u8 {
